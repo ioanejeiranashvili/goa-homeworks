afa4d7a676b11848787efa7289406bd234428684
ioanejeiranashvili <167845506+ioanejeiranashvili@users.noreply.github.com>
2024-06-12 06:05:36 -0700

    Add files via upload

---
+++ goa homework/Level 08/classwork/classwork.py
@@ -0,0 +1,9 @@
+# ახალი სიტყვები 
+# hash symbol # - დიეზი #
+# declare - შექმნა
+# transaction - გადარიცხვა
+# explanation - ახსნა, განმარტება
+# temporarily - დროებით
+# disable - გაუქმება
+# case-sensitive - ქეისზე (დიდი ასო, პატარა ასო) დამოკიდებული
+# underscore - ქვედატირე _
---
+++ goa homework/Level 08/homework/homework.py
@@ -0,0 +1,6 @@
+my_name = ("ioane")
+my_number = 5
+my_name2 =("elene")
+print(my_name)
+print(my_number)
+print(my_name2)
